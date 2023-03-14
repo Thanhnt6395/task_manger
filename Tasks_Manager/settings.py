317d54d61739c0a060d2c6f1b0b6df2eb57ed524
Thanhnt6395 <thanhnt6395@gmail.com>
2023-03-14 23:48:43 +0700

    create RegisterUserView and RegisterSerializer

--- Tasks_Manager/settings.py
+++ Tasks_Manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'drf_yasg',
     'apps.users',
 ]
@@ -134,3 +136,33 @@ SWAGGER_SETTINGS = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+
+from .serializers import RegisterSerializers
 
 # Create your views here.
+class RegisterUserView(GenericViewSet):
+    serializer_class = RegisterSerializers
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self, request):
+        serializers = RegisterSerializers(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(data=serializers.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializers.errors, status=status.HTTP_400_BAD_REQUEST)
